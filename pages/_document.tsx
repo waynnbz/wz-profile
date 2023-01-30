bb98f231071da5763c584d70204e94791a4f7b6f
Matt Zeng <32592605+waynnbz@users.noreply.github.com>
2023-01-31 01:46:42 +0800

    favicon update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,42 +4,35 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* <link
-          href="https://fonts.googleapis.com/css2?family=Moon+Dance"
-          rel="stylesheet"
-        /> */}
-        {/* <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat"
-          rel="stylesheet"
-        /> */}
-        {/* <link
-          href="https://fonts.googleapis.com/css2?family=Roboto&display=optional"
-          rel="stylesheet"
-        /> */}
-        {/* <link
-          href="https://fonts.googleapis.com/css2?family=Poppins&display=optional"
-          rel="stylesheet"
-        /> */}
-        {/* <link
-          href="https://fonts.googleapis.com/css2?family=League+Spartan&display=optional"
-          rel="stylesheet"
-        /> */}
-        {/* <link
-          href="https://fonts.googleapis.com/css2?family=Rubik&display=optional"
-          rel="stylesheet"
-        /> */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/images/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/images/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins&family=League+Spartan&family=Roboto&family=Rubik:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
         />
-        {/* <link
-          href="https://fonts.googleapis.com/css2?family=Open+Sans"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Dancing+Script"
-          rel="stylesheet"
-        /> */}
       </Head>
       <body>
         <Main />
